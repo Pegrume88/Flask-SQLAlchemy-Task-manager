38f287ca49f6c821bf2136a6f1767297103173a1
Pegrume88 <pegrume88@gmail.com>
2022-10-29 13:24:46 +0000

    add new task function created

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,4 +2,19 @@ document.addEventListener('DOMContentLoaded', function() {
     // sidenav initializing
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
+
+    // date picker
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+      format: "dd mmmm, yyyy",
+      i18n: {done: "Select"}
+    });
+
+    // Select innitialization
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
+   
   });
+
+ 
+ 
